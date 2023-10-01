35dcdac2e23cd515c266d0b39e1ed7e49b0b39d6
ashehe <ash96235@gmail.com>
2023-10-01 16:10:26 +0900

    feat: 로또 구입 금액 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
 package lotto;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 1. 로또 구입 금액 입력
+        System.out.println("구입금액을 입력해 주세요.");
+        try {
+            int money = Integer.parseInt(readLine());
+            if (money % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해주세요.");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        System.out.println("!");
     }
 }
