5242e625cd94c52f8d8058e9f85c89acc193b12a
ashehe <ash96235@gmail.com>
2023-10-01 16:23:39 +0900

    contribution 잘 뜨는지 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        // 1. 로또 구입 금액 입력
+        // 1. 로또 구입 금액 입력 (1000원 단위 아니면 예외 처리)
         System.out.println("구입금액을 입력해 주세요.");
         try {
             int money = Integer.parseInt(readLine());
