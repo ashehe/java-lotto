52ab377a0393b4eb339b111812003c72fee799f9
ashehe <ash96235@gmail.com>
2023-10-03 17:07:32 +0900

    feat: 당첨 번호 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,18 +2,19 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        int money;
-
         // 1. 로또 구입 금액 입력 (1000원 단위가 아니라면 예외 발생)
         System.out.println("구입금액을 입력해 주세요.");
+        int money;
+        money = Integer.parseInt(readLine());
         try {
-            money = Integer.parseInt(readLine());
             if (money % 1000 != 0) {
                 throw new IllegalArgumentException("[ERROR] 1,000원 단위로 입력해주세요.");
             }
@@ -33,7 +34,19 @@ public class Application {
             System.out.println(numbers);
         }
 
+        // 3. 당첨 번호 입력
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String InputWinningNumbers = readLine();
+        StringTokenizer tokenizer = new StringTokenizer(InputWinningNumbers, ",");
+        List<Integer> numbers = new ArrayList<>();
+
+        while(tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+            int num = Integer.parseInt(token);
+            numbers.add(num);
+        }
+
+        Lotto winningNumbers = new Lotto(numbers);
+        winningNumbers.print(numbers); // 삭제 예정
     }
-
-
 }
